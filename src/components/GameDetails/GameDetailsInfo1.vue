b483af065c2f505ee05f50c180ae50dada52eecb
molul <luismorcillo@gmail.com>
2024-01-06 19:08:13 +0100

    Update GameDetailsInfo1.vue

--- src/components/GameDetails/GameDetailsInfo1.vue
+++ src/components/GameDetails/GameDetailsInfo1.vue
@@ -38,7 +38,7 @@ const emit = defineEmits(["clickNext", "clickPrev"]);
       <Title :text="data.name || ''" size="lg" centered="true" />
 
       <!-- Info -->
-      <div class=" grid grid-cols-2 text-xs md:text-sm max-w-2xl mx-auto">
+      <div class=" grid grid-cols-2 text-xs md:text-sm w-full max-w-2xl mx-auto">
         
           <div class="font-bold">Genre</div>
           <div>{{ data.genres?.join(", ") }}</div>
