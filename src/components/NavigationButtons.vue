67e71d1305aeb8f11b9a8cc9febb10a0f79d068c
molul <luismorcillo@gmail.com>
2024-01-06 13:38:28 +0100

    feat: new components number and navigation buttons

---
+++ src/components/NavigationButtons.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { Icon } from "@iconify/vue";
+import { defineEmits } from "vue";
+defineEmits(["clickPrev", "clickNext"]);
+defineProps(["mode"]);
+</script>
+
+<template>
+  <div
+    class="z-50 bg-black opacity-50 hover:opacity-100 shadow-md rounded cursor-pointer transition-opacity duration-300"
+    :class="{
+      'absolute left-2 md:hidden': mode === 'mobile',
+    }"
+    @click="emit('clickPrev')"
+  >
+    <Icon icon="solar:alt-arrow-left-outline" class="size-10" />
+  </div>
+  <div
+    class="z-50 bg-black opacity-50 hover:opacity-100 shadow-md rounded cursor-pointer transition-opacity duration-300"
+    :class="{
+      'absolute right-2 md:hidden': mode === 'mobile',
+    }"
+    @click="emit('clickNext')"
+  >
+    <Icon icon="solar:alt-arrow-right-outline" class="size-10" />
+  </div>
+</template>
+
+<style></style>
