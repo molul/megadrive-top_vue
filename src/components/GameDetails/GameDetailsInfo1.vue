e94ff5a09b39ea7367c7db683a2eaf847ef28d2e
molul <luismorcillo@gmail.com>
2024-01-06 18:57:55 +0100

    Update GameDetailsInfo1.vue

--- src/components/GameDetails/GameDetailsInfo1.vue
+++ src/components/GameDetails/GameDetailsInfo1.vue
@@ -17,10 +17,10 @@ const emit = defineEmits(["clickNext", "clickPrev"]);
         class="relative flex items-center justify-between gap-4 h-full py-2 md:py-0"
       >
         <!-- Image -->
-        <div class="w-full lg:max-h-none">
+        <div class="w-full">
           <img
             :src="`/img/boxarts/${data.id}.webp`"
-            class="lg:w-full object-contain h-full max-h-[250px] lg:h-auto sm:max-h-none mx-auto"
+            class="lg:w-full object-contain h-full max-h-[250px] md:max-h-[500px] lg:h-auto sm:max-h-none mx-auto"
           />
         </div>
 
